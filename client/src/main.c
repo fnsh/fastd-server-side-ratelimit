be46197b737a342c3bdd1d482cfcd1bbb78c9110
David Bauer <mail@david-bauer.net>
2026-06-23 21:06:40 +0200

    client: enable packaging for Gluon

--- client/src/main.c
+++ client/src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/wait.h>
 
 #include <arpa/inet.h>
 
---
+++ contrib/client/gluon-upgrade.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/lua
+
+local uci = require('simple-uci').cursor()
+local platform = require 'gluon.platform'
+
+local limit_enabled = uci:get_bool('gluon', 'mesh_vpn', 'limit_enabled')
+local rate_downstream = uci:get('gluon', 'mesh_vpn', 'limit_ingress')
+local rate_upstream = uci:get('gluon', 'mesh_vpn', 'limit_egress')
+
+if not limit_enabled then
+	return
+end
+
+-- Need to remove simple-tc and sqm rules
+uci:delete('simple-tc', 'mesh_vpn')
+uci:delete('sqm', 'mesh_vpn')
+
+-- Add IPv6 LL to mesh-vpn interface
+uci:delete('network', 'mesh_vpn', 'ip6addr')
+uci:set_list('network', 'mesh_vpn', 'ip6addr', {'fe80::f421:d:2/64'})
+
+-- Configure own rules
+uci:section('fssrl', 'fssrl', 'vpn', {
+	enabled = '1',
+	interface = 'mesh-vpn',
+	minimum_downstream = rate_downstream,
+	minimum_upstream = rate_upstream,
+	maximum_downstream = rate_downstream,
+	maximum_upstream = rate_upstream,
+	script = '/usr/share/fssrl/apply-rate-limit.sh',
+	target = platform.get_target(),
+	subtarget = platform.get_subtarget(),
+})
+
+uci:save('network')
+uci:save('simple-tc')
+uci:save('sqm')
+uci:save('fssrl')
